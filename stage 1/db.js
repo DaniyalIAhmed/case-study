a23e6d260b5d2f420c04582edce3006cb41dd069
DaniyalIAhmed <m.daniyal0201@gmail.com>
2025-04-04 15:13:44 +0500

    Setup

--- stage 1/db.js
+++
@@ -1,8 +0,0 @@
-// import { Sequelize } from 'sequelize';
-
-// const sequelize = new Sequelize('postgres://postgres:3421@localhost:4601/stores', {
-//   dialect: 'postgres',
-//   logging: false,
-// });
-
-// export default sequelize;
